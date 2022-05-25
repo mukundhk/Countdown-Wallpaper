81da5ab24a3e39345b5ab96cb9f23416197f9710
Mukund Harikumar <mukundhk.2010@gmail.com>
2022-05-25 23:45:47 +0400

    fixed alignment to center

--- main.py
+++ main.py
@@ -20,14 +20,15 @@ rem_days = days%7
 
 line1 = f"Just {days} days left"
 line2 = f"{weeks} weeks and {rem_days} days" 
+text = f"""{line1}
+{line2}"""
 
-text = f"{line1}\n{line2}"
 textfont = ImageFont.truetype("assets\\Nunito-VariableFont_wght.ttf", 50)
 
 width, height = 1920, 1080
 image = Image.new("RGBA",(width,height),"black")
 
 draw = ImageDraw.Draw(image)
-draw.text((width/2, height/2), text, font=textfont, anchor="mm")
+draw.text((width/2, height/2), text, font=textfont, anchor="mm", align="center")
 
 image.save("test.png", "PNG")
