be8f4d84eed39472e2f485615739a847b65469ab
Mukund Harikumar <mukundhk.2010@gmail.com>
2022-05-26 00:11:26 +0400

    slight changes

--- main.py
+++ main.py
@@ -4,16 +4,12 @@ from PIL import Image, ImageFont, ImageDraw
 today = datetime.datetime.now()
 today_epoch = today.timestamp()
 today_text = today.strftime("%B %d, %Y")
+
 final = datetime.datetime(2022, 8, 1, 0, 0)
 final_epoch = final.timestamp()
+final_text = final.strftime("%B %d, %Y")
 
 difference = int(final_epoch-today_epoch)
-
-# seconds = difference + 1
-# minutes = seconds//60 + 1
-# hours = minutes//60 + 1
-# days = hours//24 + 1
-# weeks = f"{days//7} weeks and {str(days%7)} days"
 
 days = difference//86400 + 1
 weeks = days//7
@@ -23,7 +19,7 @@ text1 = today_text
 text2 = f"""Just {days} days left
 {weeks} weeks and {rem_days} days
 for
-{final.strftime("%B %d, %Y")}"""
+{final_text}"""
 
 textfont = ImageFont.truetype("assets\\Nunito-VariableFont_wght.ttf", 50)
 
