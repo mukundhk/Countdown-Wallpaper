514ac5748f657e142544796c06e05eae4ae5ad58
Mukund Harikumar <mukundhk.2010@gmail.com>
2022-06-01 23:43:55 +0400

    added input for theme in setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@ import json
 
 print("Type the date to count down to:")
 print("Example: 1st August 2022 would be 01-08-2022(with hyphens)")
-date = input(":- ")
+date = input(":- ").lower()
+
+print("Choose between light and dark tnemed wallpapers:")
+print("Type Light/Dark")
+theme = input(":- ").lower()
 
 dictionary = {
-    "date" : date
+    "date" : date,
+    "theme" : theme
 }
 
 json_object = json.dumps(dictionary, indent = 4)
