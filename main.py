8bc37b2dbc5ecb3a7484898a6fd3e34bf8b1aad7
Mukund Harikumar <mukundhk.2010@gmail.com>
2022-06-21 21:07:59 +0400

    created main() which is to be called in setup.py

--- main.py
+++ main.py
@@ -5,96 +5,100 @@ import os
 import json
 import random
 
-def settings(option):
-    with open("settings.json","r") as settings_file:
-        settings = json.load(settings_file)
-    data = settings[option]
+def main():
 
-    if option == "date":
-        final_date_elements = data.split("-")
-        day,month,year = int(final_date_elements[0]),int(final_date_elements[1]),int(final_date_elements[2])
-        return (day,month,year)
+    def settings(option):
+        with open("settings.json","r") as settings_file:
+            settings = json.load(settings_file)
+        data = settings[option]
 
-    if option == "resolution":
-        resolution = data.split("x")
-        width, height = int(resolution[0]), int(resolution[1])
-        return (width, height)
+        if option == "date":
+            final_date_elements = data.split("-")
+            day,month,year = int(final_date_elements[0]),int(final_date_elements[1]),int(final_date_elements[2])
+            return (day,month,year)
 
-    else:
-        return data
+        if option == "resolution":
+            resolution = data.split("x")
+            width, height = int(resolution[0]), int(resolution[1])
+            return (width, height)
 
-def remaining_days():
-    day,month,year = settings("date")  
-    today_epoch = datetime.datetime.now().timestamp()
-    final_epoch = datetime.datetime(year,month,day).timestamp()
+        else:
+            return data
 
-    difference = int(final_epoch - today_epoch)
-    days = difference//86400 + 1
-    weeks = days//7
-    rem_days = days%7
+    def remaining_days():
+        day,month,year = settings("date")  
+        today_epoch = datetime.datetime.now().timestamp()
+        final_epoch = datetime.datetime(year,month,day).timestamp()
 
-    return (days,weeks,rem_days)
+        difference = int(final_epoch - today_epoch)
+        days = difference//86400 + 1
+        weeks = days//7
+        rem_days = days%7
 
-def days_to_text():
-    day,month,year = settings("date")
-    today = datetime.datetime.now().strftime("%B %d, %Y")
-    final = datetime.datetime(year, month, day).strftime("%B %d, %Y")
-    return (today,final)
+        return (days,weeks,rem_days)
 
-def text(remaining_days,days_text):
-    if remaining_days[1] == 0:
-        if remaining_days[0] == 0:
-            text = f"""The wait is finally over.
+    def days_to_text():
+        day,month,year = settings("date")
+        today = datetime.datetime.now().strftime("%B %d, %Y")
+        final = datetime.datetime(year, month, day).strftime("%B %d, %Y")
+        return (today,final)
+
+    def text(remaining_days,days_text):
+        if remaining_days[1] == 0:
+            if remaining_days[0] == 0:
+                text = f"""The wait is finally over.
 
 Today is {days_text[1]}.
 """
-        else:
-            text = f"""{days_text[0]}
-    
+            else:
+                text = f"""{days_text[0]}
+        
 Just {remaining_days[0]} days left
 for
 {days_text[1]}
 """
-    else:
-        text = f"""{days_text[0]}
+        else:
+            text = f"""{days_text[0]}
 
 Just {remaining_days[0]} days left
 {remaining_days[1]} weeks and {remaining_days[2]} days
 for
 {days_text[1]}
 """
-    return text
+        return text
 
-def pick_colors():
-    theme = settings("theme")
-    with open(r".\assets\colors.json","r") as colors_file:
-        colors = json.load(colors_file)
-    chosen_colors = random.choice(colors[theme])
-    
-    bg_color = ImageColor.getrgb(chosen_colors["background"])
-    text_color = ImageColor.getrgb(chosen_colors["text"])
+    def pick_colors():
+        theme = settings("theme")
+        with open(r".\assets\colors.json","r") as colors_file:
+            colors = json.load(colors_file)
+        chosen_colors = random.choice(colors[theme])
+        
+        bg_color = ImageColor.getrgb(chosen_colors["background"])
+        text_color = ImageColor.getrgb(chosen_colors["text"])
 
-    return (bg_color,text_color)
-    
-def create_wallpaper(bg_color,text_color,text):
-    width, height = settings("resolution")
+        return (bg_color,text_color)
+        
+    def create_wallpaper(bg_color,text_color,text):
+        width, height = settings("resolution")
 
-    image = Image.new("RGBA",(width,height),color=bg_color)
-    draw = ImageDraw.Draw(image)
+        image = Image.new("RGBA",(width,height),color=bg_color)
+        draw = ImageDraw.Draw(image)
 
-    textfont = ImageFont.truetype("assets\\Nunito-VariableFont_wght.ttf", 50)
-    draw.text((width/2,height/2), text, align="center", anchor="mm", font=textfont, fill=text_color)
+        textfont = ImageFont.truetype("assets\\Nunito-VariableFont_wght.ttf", 50)
+        draw.text((width/2,height/2), text, align="center", anchor="mm", font=textfont, fill=text_color)
 
-    image.save(r".\assets\wallpaper.png", "PNG")
+        image.save(r".\assets\wallpaper.png", "PNG")
 
-def set_wallpaper():
-    absolute_path = os.path.abspath(r".\assets\wallpaper.png")
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, absolute_path , 0)
+    def set_wallpaper():
+        absolute_path = os.path.abspath(r".\assets\wallpaper.png")
+        ctypes.windll.user32.SystemParametersInfoW(20, 0, absolute_path , 0)
 
-if __name__ == "__main__":
     remaining_days = remaining_days()
     days_text = days_to_text()
     final_text = text(remaining_days, days_text)
     bg_color,text_color = pick_colors()
     create_wallpaper(bg_color,text_color,final_text)
     set_wallpaper()
+
+if __name__ == "__main__":
+    main()
