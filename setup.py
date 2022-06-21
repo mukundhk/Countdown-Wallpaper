6cf86fb778b1ee3fc77b4fd4db5d8d03ec8d57e9
Mukund Harikumar <mukundhk.2010@gmail.com>
2022-06-21 21:08:11 +0400

    imported main.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import json
+import main
 
 print("Type the date to count down to:")
 print("Example: 1st August 2022 would be 01-08-2022(with hyphens)")
@@ -22,3 +23,5 @@ json_object = json.dumps(dictionary, indent = 4)
 
 with open("settings.json", "w") as json_file:
     json_file.write(json_object)
+
+main.main()
